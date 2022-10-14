263f108ff525ef4ef64c17506bfd31e368deb230
Christian Rivas <99852554+crivas-2020025@users.noreply.github.com>
2022-10-14 04:27:39 -0600

    Update ICta.vue

--- resources/js/Pages/Landing/utils/ICta.vue
+++ resources/js/Pages/Landing/utils/ICta.vue
@@ -1,18 +1,21 @@
 <template>
     <!-- component -->
     <div class="max-w-full flex justify-center items-center">
-        <card class="card grid grid-cols-6 w-full p-8 bg-zinc-900">
+        <card
+            class="card  grid grid-cols-6 w-full p-8 bg-zinc-900 before:content-[attr(before)]"
+        >
             <div class="col-span-4">
                 <h2 class="font-bold text-3xl text-white ml-10 mt-8">
                     ¿Necesitas Asesoría?
                 </h2>
 
-                <p class="font-light text-white text-xl ml-10 mt-5">
-                    En caso que tengas alguna duda o pregunta en particular, solamente márcanos, y con mucho gusto te responderemos.
+                <p class="font-light text-white text-xl ml-10 mt-5 text-left">
+                    En caso que tengas alguna duda o pregunta,
+                    solamente márcanos, y con mucho gusto te responderemos.
                 </p>
 
-                <button class="text-white font-semibold ml-10 mt-5 mb-8 group">
-                    Realizar consulta o cotización 
+                <button class="text-white font-semibold ml-10 mt-5 mb-8 text-left group">
+                    Realizar consulta o cotización
                     <svg
                         xmlns="http://www.w3.org/2000/svg"
                         class="inline-block h-6 w-6 group-hover:translate-x-2 transition delay-100 transition-200"
@@ -40,8 +43,5 @@
             fixed center center;
     background-size: cover;
     padding: 120px 0;
-}
-.card::before {
-    content: "";
 }
 </style>

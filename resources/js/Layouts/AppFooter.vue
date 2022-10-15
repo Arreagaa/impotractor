7d5805234735f86cff7f714806384a73406365d0
Christian Rivas <99852554+crivas-2020025@users.noreply.github.com>
2022-10-15 11:13:05 -0600

    Update AppFooter.vue

--- resources/js/Layouts/AppFooter.vue
+++ resources/js/Layouts/AppFooter.vue
@@ -2,33 +2,42 @@
     <div class="bg-zinc-900">
         <div class="max-w-2xl mx-auto text-white py-10">
             <div class="text-center">
-                <h3 class="text-3xl mb-3">Download our fitness app</h3>
-                <p>Stay fit. All day, every day.</p>
-                <div class="flex justify-center my-10">
-                    <div
-                        class="flex items-center border w-auto rounded-lg px-4 py-2 w-52 mx-2"
-                    >
+                <h3 class="text-7xl mb-3">
+                    <a href="/">
                         <img
-                            src="https://cdn-icons-png.flaticon.com/512/888/888857.png"
-                            class="w-7 md:w-8"
+                            src="/images/impotractor/impotractor-light-logo.png"
+                            alt="image"
+                            class="rounded mx-auto d-block w-80"
                         />
-                        <div class="text-left ml-3">
-                            <p class="text-xs text-gray-200">Download on</p>
-                            <p class="text-sm md:text-base">
-                                Google Play Store
-                            </p>
-                        </div>
-                    </div>
-                    <div
-                        class="flex items-center border w-auto rounded-lg px-4 py-2 w-44 mx-2"
-                    >
-                        <img
-                            src="https://cdn-icons-png.flaticon.com/512/888/888841.png"
-                            class="w-7 md:w-8"
-                        />
-                        <div class="text-left ml-3">
-                            <p class="text-xs text-gray-200">Download on</p>
-                            <p class="text-sm md:text-base">Apple Store</p>
+                    </a>
+                </h3>
+                <p class="uppercase font-bold">
+                    Tu maquinaria, nuestros repuestos.
+                </p>
+                <div class="flex justify-center mt-10">
+                    <div class="flex items-center w-auto px-4 w-52 mx-2">
+                        <div class="flex justify-center mt-10 mb-2">
+                            <a
+                                href="https://www.facebook.com/impotractorsa/"
+                                Target="_blank"
+                            >
+                                <div
+                                    class="flex items-center border w-auto rounded-lg border-yellow-400 px-4 py-2 w-52 mx-2 hover:border hover:border-white"
+                                >
+                                    <l-icon
+                                        icon="fa-brands fa-facebook"
+                                        class="text-white text-2xl"
+                                    />
+                                    <div class="text-left ml-3">
+                                        <p class="text-xs text-gray-200">
+                                            Visítanos en nuestra página de
+                                        </p>
+                                        <p class="text-sm md:text-base">
+                                            Facebook, Impotractor S.A.
+                                        </p>
+                                    </div>
+                                </div>
+                            </a>
                         </div>
                     </div>
                 </div>
@@ -37,12 +46,12 @@
                 class="mt-28 flex flex-col md:flex-row md:justify-between items-center text-sm text-gray-400"
             >
                 <p class="order-2 md:order-1 mt-8 md:mt-0">
-                    &copy; Beautiful Footer, 2021.
+                    &copy; Impotractor S.A, Guatemala 2022.
                 </p>
                 <div class="order-1 md:order-2">
-                    <span class="px-2">About us</span>
-                    <span class="px-2 border-l">Contact us</span>
-                    <span class="px-2 border-l">Privacy Policy</span>
+                    <span class="px-2">Nosotros</span>
+                    <span class="px-2 border-l">Contáctanos</span>
+                    <span class="px-2 border-l">Políticas de Privacidad</span>
                 </div>
             </div>
         </div>

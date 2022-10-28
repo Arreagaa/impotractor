7cc2c6b406de5b60a9fa777351f07832e10bf240
Christian Rivas <99852554+Arreagaa@users.noreply.github.com>
2022-10-27 18:36:53 -0600

    try

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,6 +5,13 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
+    <title>Impotractor S.A.</title>
+    <link rel="icon" href="/images/impotractor/impotractor-only-logo.png">
+    <link rel="stylesheet" href="/images/impotractor/impotractor-only-logo.png">
+
+    <!-- Fonts -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
+
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
